9a040a539e3e968c3e1103c35bbb66e9db44136b
sponge2984 <ga576895@gmail.com>
2020-04-11 22:05:18 +0800

    homepage update

---
+++ josh/portfolio.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    console.log("ready!");
+    window.onscroll = function() { homenavbarcontrol() };
+})
+
+//控制首頁navbar
+function homenavbarcontrol() {
+    if (window.scrollY != 0) {
+        $('.mynavbar').addClass('mynavbarscroll');
+    } else {
+        $('.mynavbar').removeClass('mynavbarscroll');
+    }
+}
+
+//滾動到element位置
+function scrollto(elm) {
+    $('html, body').animate({
+        scrollTop: $(elm).offset().top - 120
+    }, 500);
+    $('.navbar-toggler').click()
+}
