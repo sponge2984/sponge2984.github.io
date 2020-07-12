6ca5d20cf60b295c265ae27d6d8ae0b3b3d87659
sponge2984 <ga576895@gmail.com>
2020-07-12 13:31:55 +0800

    react-tetris build

---
+++ react-tetris/build/precache-manifest.899f2a55a2b0f8442691138a02814991.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe49035311d1b77744199c9838a86bce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b68d73b9ba0f9927a49e",
+    "url": "/static/css/main.15d9aa86.chunk.css"
+  },
+  {
+    "revision": "1ebd91f22f3478c8235e",
+    "url": "/static/js/2.ba3b6ec1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ba3b6ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68d73b9ba0f9927a49e",
+    "url": "/static/js/main.026c2afb.chunk.js"
+  },
+  {
+    "revision": "ecb2f72e6d59aeac38f4",
+    "url": "/static/js/runtime-main.9efd397e.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
