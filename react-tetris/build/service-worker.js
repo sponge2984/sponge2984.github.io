25fb96bbaabae5a3d9a86ed88ddc04b13eabb8b8
fubini4817 <fubini4817@gmail.com>
2020-07-13 22:30:16 +0800

    update

--- react-tetris/build/service-worker.js
+++ react-tetris/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.899f2a55a2b0f8442691138a02814991.js"
+  "/react-tetris/precache-manifest.74b1f6fa92b220cd769ae3593383fb6b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tetris/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
