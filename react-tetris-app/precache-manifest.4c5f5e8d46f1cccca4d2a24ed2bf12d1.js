11a34bab16ee1972e5348813ef6ea9b221214dbe
fubini4817 <fubini4817@gmail.com>
2020-07-13 22:40:43 +0800

    test

---
+++ react-tetris-app/precache-manifest.4c5f5e8d46f1cccca4d2a24ed2bf12d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6166f6eede0750016c7e402831d37fd3",
+    "url": "/react-tetris-app/index.html"
+  },
+  {
+    "revision": "c25b91cc37fbd0708d61",
+    "url": "/react-tetris-app/static/css/main.f626d646.chunk.css"
+  },
+  {
+    "revision": "8cc4c492db329752add2",
+    "url": "/react-tetris-app/static/js/2.f4ba0aa6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tetris-app/static/js/2.f4ba0aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25b91cc37fbd0708d61",
+    "url": "/react-tetris-app/static/js/main.c786bbcb.chunk.js"
+  },
+  {
+    "revision": "31726658dcbbd3605074",
+    "url": "/react-tetris-app/static/js/runtime-main.083c5e10.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris-app/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris-app/static/media/bg.2810fe6b.png"
+  }
+]);
